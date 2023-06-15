2eb9b2149482e8c468b50b5bdec34170ae543ffc
Ashish Upadhyay <81767933+ashishu8888@users.noreply.github.com>
2023-06-15 20:59:12 +0530

    Update 2.8 detect_loop_in_LL.cpp

--- 2.8 detect_loop_in_LL.cpp
+++ 2.8 detect_loop_in_LL.cpp
@@ -9,52 +9,39 @@
 class Solution {
 public:
     ListNode *detectCycle(ListNode *head) {
-        ListNode* f = head, *s = head;
 
-        while(s)
+        ListNode* s = head,*f=head;
+        bool ans = false;
+        while(s!=NULL)
         {
             
-            
-
+        
             s = s->next;
             if(f and f->next)
-            {
-                f = f->next->next;
-            }
+            f = f->next->next;
 
             if(s == f)
             {
                 break;
             }
-
+            
+            
         }
-
         if(s == NULL)
-        {
-            return NULL;
-        }
+        return s;
 
-        
         s = head;
 
-        while(s)
+        while(s!=f)
         {
-
-             if(s == f)
-            {
-                break;
-            }
-           
-
-            s = s->next;
+            s=s->next;
             if(f)
-            f = f->next;
-
-            
-
+            f =f->next;
         }
+        
 
         return s;
+
 
 
     }
